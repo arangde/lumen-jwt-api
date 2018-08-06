5ebe6adc2427989aac840a948d06ccf1d1ae9baa
arangde <jaros.anger@gmail.com>
2018-08-06 13:35:27 +0300

    update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(MembersTableSeeder::class);
         $this->call(RefersTableSeeder::class);
         $this->call(RedeemsTableSeeder::class);
+        $this->call(AnnouncementViewsTableSeeder::class);
 
         Model::reguard();
     }
