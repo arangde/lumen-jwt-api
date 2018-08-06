95da45e797ea8c8e7392be30bdb3127e48ad170e
arangde <jaros.anger@gmail.com>
2018-08-06 16:49:02 +0300

    settings dump

---
+++ database/seeds/settings.sql
@@ -0,0 +1,13 @@
+/*Data for the table `settings` */
+
+insert  into `settings`(`id`,`setting_field`,`value`,`created_at`,`updated_at`,`name`) values 
+(1,'project_title','Membership management system','2018-07-24 06:59:15','2018-07-24 06:59:15','Project Title'),
+(17,'direct_bonus_income','500','2018-07-31 12:31:24','2018-07-31 12:31:24','Direct bonus income when a member is recommended by new one.'),
+(18,'point_rate','1','2018-07-31 12:57:56','2018-07-31 12:57:56','Rate of the additional points of when a member adds an income. (percentage in number)'),
+(19,'recurring_income','330','2018-07-31 13:31:55','2018-07-31 13:31:55','Recurring incomes'),
+(20,'recurring_income_rate','1','2018-08-01 11:18:00','2018-08-01 11:18:00','Rate of the additional incomes of recurring periods. (percentage in number)'),
+(21,'recommends_number_low','5','2018-08-01 11:43:36','2018-08-01 19:11:27','The low number of recommended members'),
+(22,'recommends_rate_low','2','2018-08-01 11:45:06','2018-08-01 11:45:06','The low rate of recommended members (percentage in number)'),
+(23,'recommends_number_high','10','2018-08-01 11:45:32','2018-08-01 11:45:32','The high number of recommended members'),
+(24,'recommends_rate_high','4','2018-08-01 11:46:01','2018-08-01 11:46:01','The high rate of recommended members (percentage in number)'),
+(25,'recurring_periods','12','2018-08-01 18:20:37','2018-08-01 18:20:37','Total recurring periods');
