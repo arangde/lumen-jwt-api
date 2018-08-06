963db48e844de9a4f36016baa154e9d7c73e2cc6
arangde <jaros.anger@gmail.com>
2018-08-06 16:39:46 +0300

    update api root

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return null;
 });
 
 $router->post('login', ['uses' => 'AuthController@authenticate']);
