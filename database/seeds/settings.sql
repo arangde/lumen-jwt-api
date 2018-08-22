ce3064a6d4adb4878852ccd42f4263e44868adaa
arangde <jaros.anger@gmail.com>
2018-08-22 21:47:16 +0300

    publish

--- database/seeds/settings.sql
+++ database/seeds/settings.sql
@@ -9,13 +9,14 @@ CREATE TABLE `settings` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `settings_setting_field(191)_index` (`setting_field`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `settings` */
 
 insert  into `settings`(`id`,`setting_field`,`value`,`created_at`,`updated_at`,`name`) values 
 (1,'project_title','Membership management system','2018-07-24 06:59:15','2018-07-24 06:59:15','Project Title'),
-(17,'direct_bonus_income','500','2018-07-31 12:31:24','2018-07-31 12:31:24','Direct bonus income when a member is recommended by new one.'),
+(16,'direct_bonus_income','500','2018-07-31 12:31:24','2018-07-31 12:31:24','Direct bonus income when a member is recommended by new one.'),
+(17,'product_price','3000','2018-08-22 15:41:21','2018-08-22 15:56:08','Product price'),
 (18,'point_rate','1','2018-07-31 12:57:56','2018-07-31 12:57:56','Rate of the additional points of when a member adds an income. (percentage in number)'),
 (19,'recurring_income','330','2018-07-31 13:31:55','2018-07-31 13:31:55','Recurring incomes'),
 (20,'recurring_income_rate','1','2018-08-01 11:18:00','2018-08-01 11:18:00','Rate of the additional incomes of recurring periods. (percentage in number)'),
